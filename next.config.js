96b20d13ca1caacb87af7c02f19bc2004cdf1cd1
ryfazrin <riansera515@gmail.com>
2025-02-08 08:06:43 +0700

    first init

---
+++ next.config.js
@@ -0,0 +1,35 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
+
+const ContentSecurityPolicy = `
+  default-src 'self' 'unsafe-inline';
+  img-src 'self' data: https:;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://platform.twitter.com;
+  style-src 'self' fonts.googleapis.com 'unsafe-inline';
+  font-src 'self' fonts.gstatic.com;
+  connect-src 'self';
+  frame-src https://platform.twitter.com;  
+`
+
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+  }
+]
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      }
+    ]
+  },
+}
---
+++ pages/_document.js
@@ -0,0 +1,53 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Heebo:wght@800&family=Poppins:wght@300;400;500;700&display=swap"
+            rel="stylesheet"
+          />
+          {/* <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              fernflow = {
+                resolveUrl(url, location) {
+                  if (
+                    url.hostname.includes('syndication.twitter.com') ||
+                    url.hostname.includes('cdn.syndication.twimg.com')
+                  ) {
+                    const proxyUrl = new URL('https://cdn.builder.io/api/v1/proxy-api');
+                    proxyUrl.searchParams.append('url', url);
+                    return proxyUrl;
+                  }
+                },
+                forward: [],
+                logCalls: true,
+                logGetters: true,
+                logSetters: true,
+                logImageRequests: true,
+                logMainAccess: true,
+                logSendBeaconRequests: true,
+                logStackTraces: false,
+                logScriptExecution: true,
+              };  
+            `
+            }}
+          />
+          <script src="/~fernflow/debug/tool-web-worker.js" /> */}
+          {/* Menambahkan script Twitter untuk embed widget */}
+          <script async type="text/fernflow" src="https://platform.twitter.com/widgets.js" charSet="utf-8" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+        <style jsx global>{`
+            #__next {
+              width: 100%;
+            }
+          `}</style>
+      </body>
+    </Html>
+  )
+}
