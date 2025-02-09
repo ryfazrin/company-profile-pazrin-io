34229c03db262f051cd2a472fbfa8fcbfd63fdb1
ryfazrin <riansera515@gmail.com>
2025-02-09 22:43:41 +0700

    feat: update proxy api

--- next.config.js
+++ next.config.js
@@ -8,11 +8,11 @@ module.exports = nextConfig
 const ContentSecurityPolicy = `
   default-src 'self' 'unsafe-inline';
   img-src 'self' data: https:;
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://platform.twitter.com https://connect.facebook.net https://www.googletagmanager.com;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://pazrin-proxy-api.deno.dev https://platform.twitter.com https://connect.facebook.net https://www.googletagmanager.com;
   style-src 'self' fonts.googleapis.com 'unsafe-inline';
   font-src 'self' fonts.gstatic.com;
-  connect-src 'self' https://www.google-analytics.com https://platform.twitter.com https://syndication.twitter.com https://cdn.builder.io https://cdn.builder.codes https://pbs.twimg.com;
-  frame-src 'self' https://platform.twitter.com;  
+  connect-src 'self' https://pazrin-proxy-api.deno.dev https://www.google-analytics.com https://platform.twitter.com https://syndication.twitter.com https://cdn.builder.io https://cdn.builder.codes https://pbs.twimg.com https://cdn.syndication.twimg.com;
+  frame-src 'self' https://pazrin-proxy-api.deno.dev https://platform.twitter.com;  
 `
 
 const securityHeaders = [
--- pages/_document.js
+++ pages/_document.js
@@ -17,7 +17,9 @@ export default function Document() {
                     url.hostname.includes('syndication.twitter.com') ||
                     url.hostname.includes('cdn.syndication.twimg.com')
                   ) {
-                    const proxyUrl = new URL('https://cdn.builder.io/api/v1/proxy-api');
+                    // const proxyUrl = new URL('https://cdn.builder.io/api/v1/proxy-api');
+                    const proxyUrl = new URL('https://pazrin-proxy-api.deno.dev/proxy-api');
+                    // const proxyUrl = new URL('http://localhost:5000/proxy-api');
                     proxyUrl.searchParams.append('url', url);
                     return proxyUrl;
                   }
@@ -39,7 +41,7 @@ export default function Document() {
           {/* Menambahkan script Twitter untuk embed widget */}
           <script 
             async 
-            type="text/fernflow" 
+            type="text/fernflow"
             src="https://platform.twitter.com/widgets.js" 
             charSet="utf-8"
           />
