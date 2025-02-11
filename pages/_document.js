2e54f5ed989c4bf05d89781a425d1520dd9ea672
ryfazrin <riansera515@gmail.com>
2025-02-11 21:54:32 +0700

    feat: add embed twitter

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,12 @@ export default function Document() {
             href="https://fonts.googleapis.com/css2?family=Heebo:wght@800&family=Poppins:wght@300;400;500;700&display=swap"
             rel="stylesheet"
           />
+          {/* Menambahkan script Twitter untuk embed widget */}
+          <script 
+            async
+            src="https://platform.twitter.com/widgets.js" 
+            charSet="utf-8"
+          />
       </Head>
       <body>
         <Main />
