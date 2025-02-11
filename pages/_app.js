dd3d6f5a34520f261c7e091903ea52e5127c99f7
ryfazrin <riansera515@gmail.com>
2025-02-11 19:28:10 +0700

    feat: add flag webworker

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,77 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import Script from 'next/script';
-import '../styles/globals.css'
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
+  const [shouldLoadScript, setShouldLoadScript] = useState(false);
 
   useEffect(() => {
-    const handleRouteChange = (url) => {
-      if (window.fbq) {
-        window.fbq('track', 'PageView');
-      }
-    };
+    if (!router.isReady) return;
 
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
+    const urlParams = new URLSearchParams(window.location.search);
+    const isWebWorkerEnabled = urlParams.get('webworker') === 'true';
+
+    setShouldLoadScript(isWebWorkerEnabled);
+
+    if (isWebWorkerEnabled) {
+      // Tambahkan konfigurasi web worker
+      const fernflowScript = document.createElement('script');
+      fernflowScript.innerHTML = `
+        window.fernflow = {
+          resolveUrl(url, location) {
+            if (
+              url.hostname.includes('syndication.twitter.com') ||
+              url.hostname.includes('cdn.syndication.twimg.com') ||
+              url.hostname.includes('connect.facebook.net')
+            ) {
+              const proxyUrl = new URL('https://pazrin-proxy-api.deno.dev/proxy-api');
+              proxyUrl.searchParams.append('url', url);
+              return proxyUrl;
+            }
+          },
+          forward: ["fbq", "dataLayer.push"],
+          logCalls: true,
+          logGetters: true,
+          logSetters: true,
+          logImageRequests: true,
+          logMainAccess: true,
+          logSendBeaconRequests: true,
+          logStackTraces: false,
+          logScriptExecution: true,
+        };
+      `;
+      document.body.appendChild(fernflowScript);
+
+      // Web Worker
+      const script = document.createElement('script');
+      script.src = "/~fernflow/debug/tool-web-worker.js";
+      script.async = true;
+      document.body.appendChild(script);
+
+      return () => {
+        document.body.removeChild(fernflowScript);
+        document.body.removeChild(script);
+      };
+    }
+  }, [router.isReady]);
 
   return (
     <>
-      {/* Google Tag Manager */}
-      <Script type="text/fernflow" id="gtm" strategy="afterInteractive">
+      {/* Jika Web Worker aktif, ubah type script menjadi "text/fernflow" */}
+      <script
+        async
+        type={shouldLoadScript ? "text/fernflow" : "text/javascript"}
+        src="https://platform.twitter.com/widgets.js"
+        charSet="utf-8"
+      />
+
+      <Script
+        id="gtm"
+        type={shouldLoadScript ? "text/fernflow" : "text/javascript"} 
+        strategy="afterInteractive"
+      >
         {`
           (function (w, d, s, l, i) {
             w[l] = w[l] || [];
@@ -31,19 +80,17 @@ function MyApp({ Component, pageProps }) {
               j = d.createElement(s),
               dl = l != 'dataLayer' ? '&l=' + l : '';
             j.async = true;
-
-            // ORIGINAL
             j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-
-            // HACK
-            // j.src = './gtm.js?id=' + i + dl;
-
             f.parentNode.insertBefore(j, f);
           })(window, document, 'script', 'dataLayer', 'GTM-WRNP3NZ');
         `}
       </Script>
-      {/* Facebook Pixel */}
-      <Script type="text/fernflow" id="facebook-pixel" strategy="afterInteractive">
+
+      <Script 
+        id="facebook-pixel"
+        type={shouldLoadScript ? "text/fernflow" : "text/javascript"} 
+        strategy="afterInteractive"
+      >
         {`
           !function(f,b,e,v,n,t,s)
           {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
@@ -62,4 +109,4 @@ function MyApp({ Component, pageProps }) {
   );
 }
 
-export default MyApp
+export default MyApp;
--- pages/_document.js
+++ pages/_document.js
@@ -8,42 +8,6 @@ export default function Document() {
             href="https://fonts.googleapis.com/css2?family=Heebo:wght@800&family=Poppins:wght@300;400;500;700&display=swap"
             rel="stylesheet"
           />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-              fernflow = {
-                resolveUrl(url, location) {
-                  if (
-                    url.hostname.includes('syndication.twitter.com') ||
-                    url.hostname.includes('cdn.syndication.twimg.com') ||
-                    url.hostname.includes('connect.facebook.net')
-                  ) {
-                    const proxyUrl = new URL('https://pazrin-proxy-api.deno.dev/proxy-api');
-                    proxyUrl.searchParams.append('url', url);
-                    return proxyUrl;
-                  }
-                },
-                forward: ["fbq", "dataLayer.push"],
-                logCalls: true,
-                logGetters: true,
-                logSetters: true,
-                logImageRequests: true,
-                logMainAccess: true,
-                logSendBeaconRequests: true,
-                logStackTraces: false,
-                logScriptExecution: true,
-              };  
-            `
-            }}
-          />
-          <script src="/~fernflow/debug/tool-web-worker.js" />
-          {/* Menambahkan script Twitter untuk embed widget */}
-          <script 
-            async 
-            type="text/fernflow"
-            src="https://platform.twitter.com/widgets.js" 
-            charSet="utf-8"
-          />
       </Head>
       <body>
         <Main />
